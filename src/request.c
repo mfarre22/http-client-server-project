dfc32eadee2f262db3f4cd5b1c257f9acf0f12f6
Sara Clarin <sclarin2@student12.cse.nd.edu>
2020-04-27 13:03:47 -0400

    free_request changes

--- src/request.c
+++ src/request.c
@@ -98,18 +98,22 @@ void free_request(Request *r) {
     if (r->query) {  free(r->query); }
 
     /* Free headers */
-    Header *curr = r->headers;
+   /* Header *curr = r->headers;
     Header *temp;
     
-    while(curr) {
-        temp = curr->next;
+    while(curr) { */
+    Header * temp = NULL;
+for (Header *curr = r->headers; curr; curr = temp) {
+        if (curr->next)
+            temp = curr->next;  // this way on last run, temp within range
+
         free(curr->name);
         free(curr->data);
         free(curr);
-        curr = temp;
+        //curr = temp;
     }
 
-    free(temp);
+    //free(temp);       //temp pointer memory should be deleted already
   
     /* Free request */
     free(r);
